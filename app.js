58825be98b3da547a7f15da9a9fe6b740b7009f4
foreman23 <foremanb@oregonstate.edu>
2023-02-20 19:15:45 -0800

    updated to reflect drug_combinations excel

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express  = require('express');
-const mysql = require('mysql');
+const { Pool } = require('pg');
 const app = express();
 const port = 3000;
 
@@ -7,12 +7,13 @@ app.listen(port, () => {
     console.log(`API server running on port ${port}`)
 })
 
-// Database credentials
-const database = mysql.createConnection({
-    host: 'host',
-    user: 'user',
-    password: 'password',
-    database: 'database'
+// Database admin credentials
+const pool = new Pool({
+    host: process.env.VET_APP_DB_HOST,
+    user: process.env.VET_APP_DB_USER,
+    password: process.env.VET_APP_DB_PASSWORD,
+    database: process.env.VET_APP_DB_DATABASE,
+    port: process.env.VET_APP_DB_PORT,
 })
 
 //database.connect();     DATABASE NOT YET IMPLEMENTED
@@ -21,62 +22,67 @@ app.get('/', (req, res) => {
     res.send('Placeholder landing page.');
 })
 
-// Retrieve information from drugs table. (skeleton code)
+// Retrieve all information from drug_combinations table. (skeleton code)
 app.get('/display_drugs', (req, res) => {
     res.send('Drugs page.');
     // PLACEHOLDER CODE BELOW
-    database.query('SELECT * FROM drug', (error, results) => {
+    pool.query('SELECT * FROM drug_combinations', (error, results) => {
         if (error) {
             res.status(500).send({error: 'Failed to retrieve data.'});
         } else {
-            res.send(results);
+            res.send(results.rows);
         }
     })
 })
 
-// Retrieve information from animals table. (skeleton code)
-app.get('/display_animals', (req, res) => {
-    res.send('Animals page.');
-    // PLACEHOLDER CODE BELOW
-    database.query('SELECT * FROM animal', (error, results) => {
-        if (error) {
-            res.status(500).send({error: 'Failed to retrieve data.'});
-        } else {
-            res.send(results);
-        }
-    })
-})
+// Retrieve all drug information pertaining to a specific animal
+app.get('/drugs/:animal_name', (req, res) => {
+    const animalName = req.params.animal_name;
+    const query = 'SELECT * FROM drug_combinations WHERE animal_name = $1';
+    const values = [animalName];
+    pool.query(query, values, (error, results) => {
+      if (error) {
+        res.status(500).send({ error: 'Failed to retrieve data.' });
+      } else {
+        res.send(results.rows);
+      }
+    });
+  });
 
-// Add entry to drugs table. (skeleton code)
+// Add entry to drug_combinations table. (skeleton code)
 app.post('/add_drug', (req, res) => {
-    const data = 'PLACEHOLDERDATA';
-    res.send('Add drug page.');
-    dataabase.query('INSERT INTO drug SET ?', data, (error, results) => {
+    const { animal_name, drug_name, method, concentration, dosage, notes } = req.body;
+
+    // User input validation
+    // check for missing required attributes
+    if (!animal_name || !drug_name || !method || !concentration || !dosage) {
+        res.status(500).send({ error: 'Missing required attributes.' });
+        return;
+    }
+
+    // check for invalid data types
+    if (typeof animal_name !== 'string' || typeof drug_name !== 'string' || typeof method !== 'string' || typeof concentration !== 'string' || typeof dosage !== 'number') {
+        res.status(500).send({ error: 'Invalid data type for attributes.' });
+        return;
+    }
+
+    // create query to send to database
+    const query = 'INSERT INTO drug_combinations(animal_name, drug_name, method, concentration, dosage, notes) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
+    const values = [animal_name, drug_name, method, concentration, dosage, notes];
+
+    pool.query(query, values, (error, results) => {
         if (error) {
-            res.status(500).send({error: 'Failed to update drug data.'});
+            res.status(500).send({ error: 'Failed to insert data.' });
         } else {
-            res.send(results);
+            res.send(results.rows[0]);
         }
     })
 })
 
-// Add entry to animal table. (skeleton code)
-app.post('/add_animal', (req, res) => {
-    const data = 'PLACEHOLDERDATA';
-    res.send('Add animal page.');
-    dataabase.query('INSERT INTO animal SET ?', data, (error, results) => {
-        if (error) {
-            res.status(500).send({error: 'Failed to update animal data.'});
-        } else {
-            res.send(results);
-        }
-    })
-})
-
-// Delete entry from drug table. (skeleton)
+// Delete entry from drug_combinations table. (skeleton)
 app.delete('/delete_drug/:id', (req, res) => {
     const id = req.params.id;
-    database.query('DELETE FROM drug WHERE id = ?', [id], (error, results) => {
+    pool.query('DELETE FROM drug_combinations WHERE id = $1', [id], (error, results) => {
         if (error) {
             res.status(500).send({error: 'Failed to delete drug data.'});
         } else {
@@ -85,42 +91,26 @@ app.delete('/delete_drug/:id', (req, res) => {
     })
 })
 
-// Delete entry from animal table. (skeleton)
-app.delete('/delete_animal/:id', (req, res) => {
-    const id = req.params.id;
-    database.query('DELETE FROM animal WHERE id = ?', [id], (error, results) => {
-        if (error) {
-            res.status(500).send({error: 'Failed to delete animal data.'});
-        } else {
-            res.send(results);
-        }
-    })
-})
-
-// Modify entry in drug table. (skeleton)
+// Modify entry in drug_combinations table. (skeleton)
 app.put('/modify_drug/:id', (req, res) => {
     const id = req.params.id;
     // new data to be updated
-    const data = req.body;
+    const { animal_name, drug_name, method, concentration, dosage, notes } = req.body;
 
-    database.query(`UPDATE drug SET ? WHERE id = ${id}`, data, (error, results) => {
+    // User input validation
+    // check for invalid data types
+    if (typeof animal_name !== 'string' || typeof drug_name !== 'string' || typeof method !== 'string' || typeof concentration !== 'string' || typeof dosage !== 'number') {
+        res.status(500).send({ error: 'Invalid data type for attributes.' });
+        return;
+    }
+    
+    // create query to send to database
+    const query = 'UPDATE drug_combinations SET animal_name = $1, drug_name = $2, method = $3, concentration = $4, dosage = $5, notes = $6 WHERE id = $7';
+    const values = [animal_name, drug_name, method, concentration, dosage, notes, id]
+
+    pool.query(query, values, (error, results) => {
         if (error) {
             res.status(500).send({error: 'Failed to update drug data.'});
-        } else {
-            res.send(results);
-        }
-    })
-})
-
-// Modify entry in animal table. (skeleton)
-app.put('/modify_animal/:id', (req, res) => {
-    const id = req.params.id;
-    // new data to be updated
-    const data = req.body;
-
-    database.query(`UPDATE animal SET ? WHERE id = ${id}`, data, (error, results) => {
-        if (error) {
-            res.status(500).send({error: 'Failed to update animal data.'});
         } else {
             res.send(results);
         }
