0eac9df2033d401e2343d54a7569553243ab6f89
Liam <mailman1276@gmail.com>
2023-03-03 16:06:27 -0800

    added some documenation using swagger. added cors. moved some files around to be in more sensible location, i.e., only index.js at root

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import cors from 'cors';
+
+import swagger_ui from 'swagger-ui-express';
+import fs from 'fs';
+import YAML from 'yaml';
 
 import auth from 'middlewares/auth.middleware.js';
 import error from 'middlewares/general_error.middleware.js';
@@ -12,15 +17,12 @@ import methods from 'routes/methods.route.js';
 import units from 'routes/units.route.js';
 
 const app = express();
+app.use(cors()); // neeeded for programmtic acces in frontend
 
 app.use(express.urlencoded({extended: true})); // for application/x-www-form-urlencoded
 app.use(express.json()); // for application/json
 
 app.use(auth);
-
-app.get('/', (req, res) => {
-    res.send({"message": "You've reached the vaddb rest api"});
-})
 
 app.use('/animals', animals);
 app.use('/concentrations', concentrations);
@@ -29,6 +31,15 @@ app.use('/dosages', dosages);
 app.use('/drugs', drugs);
 app.use('/methods', methods);
 app.use('/units', units);
+
+const file = fs.readFileSync("swagger.yaml", "utf8");
+const swagger_document = YAML.parse(file)
+const swagger_options = {
+    swaggerOptions: {
+        defaultModelRendering: 'model'
+    }
+};
+app.use('/api-docs', swagger_ui.serve, swagger_ui.setup(swagger_document, swagger_options));
 
 app.use(error);
 
